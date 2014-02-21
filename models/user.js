4ae6a9fc83a04137a68281bdc13eece41b1c109e
Siddharth Choudhary <siddharthchoudhary@gatech.edu>
2014-02-21 06:58:45 -0500

    UserSchema implemented

--- models/user.js
+++ models/user.js
@@ -11,13 +11,54 @@ var UserSchema =  new Schema({
 			unique: true
 		},
 	password: {
-		type: String,
+		tiype: String,
 		required: true
-	}
+		},
 	email: {
 		type: String,
 		required: true
+		},
+	isAdminFlag: {
+		type: Boolean,
+		require: true
+		}
+});
+
+UserSchema.pre('save', function(next){
+	var user = this;
+
+	// only hash the password if it has been modified or new
+	if(!user.isModified('password')){
+		return next();
 	}
 
-}
+	//generate a salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+		if(err){
+			return next(err);
+		}
+
+		//hash the password using new salt
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if(err){
+				return next(err);
+			}
+
+			//override cleartext password with hashed
+			user.password = hash;
+			next();
 		});
+	});
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword){
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+		if(err){
+			return cb(err);
+		}
+
+		cb(null, isMatch);
+	});	
+};
+
+module.exports = mongoose.model("User", UserSchema);
