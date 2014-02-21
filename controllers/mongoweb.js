c6477a3b1949f571ea9389cbad06b05373037ca8
palak <no.1palak@gmail.com>
2014-02-21 19:30:49 +0530

    controllers added

--- controllers/mongoweb.js
+++ controllers/mongoweb.js
@@ -3,27 +3,50 @@ var User = require("../models/user.js");
 var MongoWebController = {
 	
 	configure: function(){	//admin account init check
-		User.findOne({
-				username: "admin"
-			}, function(err, result){
-				if (err){
-					throw err;
-				}
-
-				if (!result){
-					new User({
-							username: "admin",
-							password: "adminadmin",
-							email: "admin@domain.com",
-							isAdminFlag: "true"
-						}).save(function(err, result){
-							if(err){
+		User.findOne({username: "admin"}, function(err, result){
+			if (err){
+				throw err;
+			}
+			if (!result){
+				new User({
+					username: "admin",
+					password: "adminadmin",
+					email: "admin@domain.com",
+					isAdminFlag: "true"
+				}).save(function(err, result){
+					if(err){
 								
-							}
-						});
-				}
+					}
+				});
 			}
 		});
+	},
+
+	helper : {
+		authenticate : function(req, res, next) {
+			var isAuthenticated = false;
+			if (req.session.username) {
+				isAuthenticated = true;
+			}
+			if (isAuthenticated) {
+				next();
+			} 
+			else {
+				console.log("Authentication error");
+				res.redirect('/login');
+			}
+		},			      
+		authenticateAdmin : function(req, res, next) {
+			var isAuthenticated = false;
+			if (req.session.username && req.session.isAdminFlag === true)
+				isAuthenticated = true;
+			if (isAuthenticated)
+				next();
+			else {
+				console.log("Admin verification failed");
+				res.redirect('/login');
+			}
+		}
 	},
 
 	loginPage : function(req, res) {
@@ -63,6 +86,85 @@ var MongoWebController = {
 		req.session.destroy();
 		res.status(302).redirect("/login");
 	},
+
+	home: function(req, res, next){
+		res.sendfile("messages.html", {root: "./views/"});
+	},
+
+	registerPage: function(req, res){
+		res.sendfile("register.html", {root: "./views/"});
+		req.session.lastPage = '/login';
+	},
+	
+	register: function(req, res, next){
+		var username = req.param("username", "");
+		var password = req.param("password", "");
+		var email = req.param("email", "");
+		var isAdminFlag = req.param("isAdminFlag", "");
+
+		if (username == 'Username'){
+			res.send("Please input a valid username");
+			return;
+		}
+
+		if (username = 'mail@address.com'){
+			res.send("Please input a valid email address");
+			return;
+		}
+
+		if (isAdminFlag === ""){
+			isAdminFlag = false;
+		}
+
+		User.findOne({
+			email: email
+		}, function(err, result){
+			if (err){
+				throw err;
+			}
+
+			if (result){
+				res.send("This email id  is already registered...\nPlease use Reset Form for password recovery.");
+			}
+			else{
+				new User({
+					username: username,
+					password: password,
+					email: email,
+					isAdminFlag: isAdminFlag
+				}).save(function(err, result){
+					if(err){
+						throw wrr;
+					}
+
+					var mailOptions = {
+						from: "MongoWeb Dev Team <mongoweb.node@gmail.com>",
+						to: email,
+						subject: "Hello World",
+						text: "Hello world",
+						html: "<b>Hello world.</b>"
+					};
+
+					var smtpTransport = nodemailer.createTransport("SMTP", {
+						service: "Gmail",
+						auth: {
+							user: "mongoweb.node@gmail.com",
+							pass: ""
+						}
+					});
+
+					smtpTransport.sendMail(mailOptions, function(error, response){
+						if(error){
+							console.log("Invalid Email: "+error);
+							res.redirect('/register');
+						}
+					});
+
+					res.send("Please verify through the email received.");
+				});
+			}
+		});
+	}
 };
 
 module.exports = MongoWebController;
