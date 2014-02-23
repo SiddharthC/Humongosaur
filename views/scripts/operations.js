2b6e7241b0a5c688bbe0e40e4ce1a84f7e9c9a00
palak <no.1palak@gmail.com>
2014-02-23 16:38:27 +0530

    run button working

--- views/scripts/operations.js
+++ views/scripts/operations.js
@@ -1,5 +1,6 @@
 //run button
-$('#run_button').click(function(){
+/*$('#run_button').click(function(){
+
 	var params = {
 		query: jQuery('#input_query_box').val()
 	};
@@ -7,4 +8,16 @@ $('#run_button').click(function(){
 	$.getJSON("/inputQuery", params, function(result){
 		jQuery('#output_box').val() = result;
 	});
-});
+});*/
+
+function myFunction()
+{
+	document.getElementById("demo").innerHTML="Hello World";
+	var params = {
+		query: jQuery('#input_query_box').val()
+	};
+
+	$.getJSON("/inputQuery", params, function(result){
+		jQuery('#output_box').val() = result;
+	});
+}
